89f533e9e3bad6288d928fa67b04b8fda30ca1fd
Lauren Pardee <lap5151@gmail.com>
2019-02-18 17:01:57 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,12 +9,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     roles = []
-    #characters = []
     self.shows.each do |show|
-      #all_shows << show.name
-      #show_name = show.name
-    #  role =  Character.find_by(show_id: show.id)
-    #  role_name = role.name
       roles << "#{Character.find_by(show_id: show.id).name} - #{show.name}"
     end
     roles
