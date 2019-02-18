e5041bd63682ff68c0f9da557ac7af333945b7dc
Lauren Pardee <lap5151@gmail.com>
2019-02-18 14:22:00 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 6) do
+ActiveRecord::Schema.define(version: 7) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -31,7 +31,8 @@ ActiveRecord::Schema.define(version: 6) do
   end
 
   create_table "shows", force: :cascade do |t|
-    t.string "name"
+    t.string  "name"
+    t.integer "network_id"
   end
 
 end
