f58298b391796a8457f465f6bf9c3a573dcf2d06
Lauren Pardee <lap5151@gmail.com>
2019-02-18 14:38:45 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,7 +9,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     #self.shows.each do |show|
-    cid =   Actors.find_by(actor_id: 1)
+    cid =   Show.where(actor_id: 1)
       binding.pry
   end
 
