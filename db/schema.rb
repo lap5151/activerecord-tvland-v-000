8a6cbec01ddce7f9e39aecd2b42fa81d28b12605
Lauren Pardee <lap5151@gmail.com>
2019-02-18 14:20:11 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,15 +14,15 @@
 ActiveRecord::Schema.define(version: 6) do
 
   create_table "actors", force: :cascade do |t|
-    t.string  "first_name"
-    t.string  "last_name"
-    t.integer "actor_id"
+    t.string "first_name"
+    t.string "last_name"
   end
 
   create_table "characters", force: :cascade do |t|
     t.string  "name"
     t.integer "show_id"
     t.string  "catchphrase"
+    t.integer "actor_id"
   end
 
   create_table "networks", force: :cascade do |t|
