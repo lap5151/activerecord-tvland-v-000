a56f49a851e6d31f19f730df93b79358757597d4
Lauren Pardee <lap5151@gmail.com>
2019-02-18 19:55:07 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,9 +2,9 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :networks
 
-  def build_network(network)
+  #def build_network(network)
     #self.network.call_letters = network.name
     #binding.pry
-  end
+#  end
 
 end
